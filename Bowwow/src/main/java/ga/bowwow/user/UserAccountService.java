c3ee9b27912ae4c9531b88929f3b2f66b5a4c956
Lan9erhans <lan9erhans@gmail.com>
2021-03-22 11:47:18 +0900

    add Dependency Lombok and Tiles

--- Bowwow/src/main/java/ga/bowwow/user/UserAccountService.java
+++ Bowwow/src/main/java/ga/bowwow/user/UserAccountService.java
@@ -5,11 +5,10 @@ import java.util.List;
 import ga.bowwow.user.UserAccount;
 
 public interface UserAccountService {
-	//CRUD 기능 구현 메소드 정의
 	void insertUserAccount(UserAccount userAccount);
-	void updateUserAccount(UserAccount userAccount );
-	void deleteUserAccount(UserAccount userAccount );
+//	void updateUserAccount(UserAccount userAccount );
+//	void deleteUserAccount(UserAccount userAccount );
 	UserAccount getUserAccount(UserAccount userAccount );
-	List<UserAccount> getUserAccountList();
-	List<UserAccount> getUserAccountList(UserAccount userAccount );
+//	List<UserAccount> getUserAccountList();
+//	List<UserAccount> getUserAccountList(UserAccount userAccount );
 }
--- Bowwow/src/main/java/ga/bowwow/user/UserAccountServiceImpl.java
+++ Bowwow/src/main/java/ga/bowwow/user/UserAccountServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ViewResolver;
 
 //@Service : @Component 상속확장 어노테이션
 //		비즈니스 로직처리 서비스 영역에 사용
@@ -12,31 +13,19 @@ public class UserAccountServiceImpl implements UserAccountService {
 	@Autowired //타입이 일치하는 객체(인스턴스) 주입
 	//private UserAccountDAO UserAccountDAO;
 	//private UserAccountDAOSpring UserAccountDAO;
-	
-	ViewResolver vr = new ViewResolver;
+//	ViewResolver vr = new ViewResolver;
 	private UserAccountDAO UserAccountDAO;
 	
 	public UserAccountServiceImpl() {
 		System.out.println(">> UserServiceImpl() 객체생성");
 	}
-	
+
 	@Override
-	public void insertUserAccount(UserAccount vo) {
-		UserAccountDAO.insertUserAccount();
+	public void insertUserAccount(UserAccount userAccount) {
 	}
 
 	@Override
-	public void updateUserAccount(UserAccount vo) {
-		UserAccountDAO.updateUserAccount();
-	}
-
-	@Override
-	public void deleteUserAccount(UserAccount vo) {
-		UserAccountDAO.deleteUserAccount();
-	}
-
-	@Override
-	public UserAccount getUserAccount(UserAccount vo) {
-		return UserAccountDAO.getUserAccount();
+	public UserAccount getUserAccount(UserAccount userAccount) {
+		return null;
 	}
 }
