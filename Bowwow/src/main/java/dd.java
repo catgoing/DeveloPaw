8d5cc293cad1ff2c958b31cc8d5686b4d142f725
kkttyy225 <73806136+kkttyy225@users.noreply.github.com>
2021-03-22 16:23:12 +0900

    lombok

---
+++ Bowwow/src/main/java/dd.java
@@ -0,0 +1,11 @@
+import ga.bowwow.board.Board;
+
+public class dd {
+
+	public static void main(String[] args) {
+		Board board = new Board();
+		board.setArea("ty");
+		System.out.println(board.getArea());
+	}
+
+}
