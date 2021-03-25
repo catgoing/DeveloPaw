0969b16152710d7693350dab7b3c7a44e5e84d96
LSHyeon <ttotto4417@naver.com>
2021-03-25 18:29:38 +0900

    test

--- Bowwow/src/main/java/ga/bowwow/service/store/Cartlist.java
+++ Bowwow/src/main/java/ga/bowwow/service/store/Cartlist.java
@@ -15,5 +15,5 @@ public class Cartlist {
 	private int cart_id;			// 장바구니 번호
 	private int member_serial;		// 회원 번호
 	private int p_id;				// 상품 번호
-	private int amount;				// 장바구니에 담긴 수량
+	private int amount;				 // 장바구니에 담긴 수량
 }
